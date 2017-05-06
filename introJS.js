81764de6c3a798185150f0ca813478294aff73b2
aapokinnari <aapo.kinnari@gmail.com>
2017-05-06 22:12:58 +0300

    Uus intro

--- introJS.js
+++ introJS.js
@@ -1,24 +1,29 @@
 
 var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'intro-test', { preload: preload, create: create });
 
-function preload() {
-
-    game.load.image('testButton', 'assets/testButton.png')
-    game.load.image('background','assets/introBGTest.png');
-
+function preload(){
+    game.load.image('menuscreen','assets/menubackground.png')
+    game.load.spritesheet('button', 'assets/buttons.png', 193 , 71);
 }
 
-var button;
-var background;
+function create(){
+game.add.tileSprite(0, 0, 1000, 600, 'menuscreen');    
+button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
+    button.onInputOver.add(over, this);
+    button.onInputOut.add(out, this);
+    button.onInputUp.add(up, this);
+}  
+function up() {
+    console.log('button up', arguments);
+}
 
-function create() {
+function over() {
+    console.log('button over');
+    console.log(gameState);
+}
 
-
-    background = game.add.tileSprite(0, 0, 1000, 600, 'background');
-
-    button = game.add.button(game.world.centerX - 95, 400, 'testButton', actionOnClick, this, 2, 1, 0);
-
-
+function out() {
+    console.log('button out');
 }
 
 
